a331a938ac530202f7f0b411f30da79e989a7a65
sebastianfiuba <seandrada@fi.uba.ar>
2025-03-19 23:24:47 +0000

    Message

--- main.cpp
+++ main.cpp
@@ -1,23 +1,25 @@
-#include "mbed.h"
 #include "arm_book_lib.h"
+#include "mbed.h"
 
-int main()
-{
-    DigitalIn gasDetector(D2);
+int main() {
+  DigitalIn gasDetector(D2);
 
-    DigitalOut alarmLed(LED1);
+  DigitalOut alarmLed(LED1);
 
-    gasDetector.mode(PullDown);
+  gasDetector.mode(PullDown);
 
-    alarmLed = OFF;
+  alarmLed = OFF;
+  
+  printf("%s\n", "Hello World");
 
-    while (true) {
-        if ( gasDetector == ON ) {
-            alarmLed = ON;
-        }
-        
-        if ( gasDetector == OFF ) {
-            alarmLed = OFF;
-        }
+  while (true) {
+    if (gasDetector == ON) {
+      alarmLed = ON;
     }
+
+    if (gasDetector == OFF) {
+      alarmLed = OFF;
+    }
+
+  }
 }

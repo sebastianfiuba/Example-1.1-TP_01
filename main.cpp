f629b7113f1e9b11f9914d1019db9f6fb37be97a
sebastianfiuba <seandrada@fi.uba.ar>
2025-03-20 00:39:10 +0000

    MessageC

--- main.cpp
+++ main.cpp
@@ -1,16 +1,36 @@
+/*! 
+ *  \brief     Activa una alarma cuando se detecta gas
+ *  \details   El gas se detecta a partir del pin D2 y se activa la alarma con LED1. Se manda un mensaje de inicio por el puerto serie
+ *  \authors   Sebastian Andrada, Sergio Aguirre
+ *  \date      19/03/2025
+ */
+
 #include "arm_book_lib.h"
 #include "mbed.h"
 
+
+/*!
+    \param none
+*/
 int main() {
   DigitalIn gasDetector(D2);
 
   DigitalOut alarmLed(LED1);
+/*!
+    \note { Se pone el pin D2 como PullDown}
 
+*/
   gasDetector.mode(PullDown);
 
   alarmLed = OFF;
-  
+
+/*!
+    \note { Se manda el siguiente mensaje para saber que se inicializo el programa }
+*/
+
   printf("%s\n", "Hello World");
+
+
 
   while (true) {
     if (gasDetector == ON) {
